12958ca2e2e5d03d45f87c0cf2d7c93a91c542df
hiteshhilok <hiteshhilok@gmail.com>
2013-01-08 12:41:59 +0530

    velaro code remove all the pages

--- web/wp/wp-content/themes/Auberge/footer.php
+++ web/wp/wp-content/themes/Auberge/footer.php
@@ -92,7 +92,6 @@ document.write('<script src="'+sm+'"></scr'+'ipt>');
 </script>
 <!-- End Velaro Weblink Code -->
 <?php  }  ?> 
-<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#username=aubergescom"></script> 
 </body>
 </html>
 <?php } ?>
