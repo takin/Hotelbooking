d1ecd66825151db8c92a57e683508f4b50db8b83
jcurlier <jeromecurlier@mcwebmanagement.com>
2013-01-29 13:08:12 -0800

    Update code/application/models/microsoft/microsofttranslator.php
    put back batchTranslate

--- code/application/models/microsoft/microsofttranslator.php
+++ code/application/models/microsoft/microsofttranslator.php
@@ -478,8 +478,6 @@ log_message("debug"," Translation language From ".$this->FromLang.' To '. $this-
 
   function batch_translate()
   {
-  	
-    return NULL;
 
     if(count($this->batchText) > 0 )
     {
