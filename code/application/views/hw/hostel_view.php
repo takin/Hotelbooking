86d009cfe8bc7db5e30d04ea8089e1f9ab1cca3c
Jerome Curlier <jeromecurlier@mcwebmanagement.com>
2013-02-24 15:51:46 -0800

    MCWEB-365 - Javascript date error

--- code/application/views/hw/hostel_view.php
+++ code/application/views/hw/hostel_view.php
@@ -361,10 +361,9 @@ endforeach; ?>
 									if(isValidDate(date_cookie))
 									{
 										var date_url = date_cookie;
-
-										var date_avail 	= new Date(date_cookie.replace('-',',','g'));
+										var date_array = date_cookie.split('-');
+										var date_avail 	= new Date(date_array[0],date_array[1]-1,date_array[2]);
 										$("#book-pick").datepicker( "setDate" , date_avail );
-
 									}
 									else
 									{
@@ -551,18 +550,18 @@ endforeach; ?>
 				</div>
 				<div class="content_block">
 					<h2><?php echo _("Cartes");?></h2>
-                                        <?php  
-                                             if (is_array($district_info) && !empty($district_info)) 
+                                        <?php
+                                             if (is_array($district_info) && !empty($district_info))
                                                  { ?>
                                                  <div id="hostel_mapView_districts" class="hostel_mapView_districts">
                                                     <p>
                                              <?php echo _('Districts');?>:
-                                             
+
                                                  <?php
-                                                 foreach ($district_info as $key => $district) 
+                                                 foreach ($district_info as $key => $district)
                                                      {
                                                       $checked = "";
-                          
+
                                                      if ($key == 0) {
                                                          $checked = "checked";
                                                      }
@@ -571,10 +570,10 @@ endforeach; ?>
                                                       <input type="radio" id="distrinct" name="distrinct" <?php echo $checked; ?> value="<?php echo $district->um_id; ?>"
                                                   onchange="changeDistrictLayer(<?php echo $district->um_id; ?>);"><?php echo $district->district_name; ?>
 
-                                            <?php  }//end Foreach  ?>                                        
+                                            <?php  }//end Foreach  ?>
                                                          </p>
-                                             </div> 
-                                                          
+                                             </div>
+
                                               <?php   }// end if ?>
 					<div id="map-wrap" class="margbot20">
 						<div id="map_canvas"></div>
@@ -668,11 +667,11 @@ if ($this->uri->segment(4, 0)) {
     $uri_segement = strtolower($this->uri->segment(4));
     if ($uri_segement == "map") {
         // make the diection tab selected and triger the click event
-        echo "<script type='text/javascript'>$(document).ready(function() { $('#hostels_tabs').tabs('select',1); $('#show_full_map').trigger('click'); });</script>";   
+        echo "<script type='text/javascript'>$(document).ready(function() { $('#hostels_tabs').tabs('select',1); $('#show_full_map').trigger('click'); });</script>";
     } else if ($uri_segement == 'comments') { // make the coments tab selected
         echo "<script type='text/javascript'>$(document).ready(function() { $('#hostels_tabs').tabs('select',2); });</script>";
     } else {
         // do nothing
     }
 }
-?> 
+?>
