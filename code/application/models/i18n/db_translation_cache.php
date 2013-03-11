9841a41eecf654e30cbdaa308e8e426eade35357
Jerome Curlier <jcurlier@yahoo.com>
2013-03-11 16:07:44 -0700

    Update db_translation_cache.php

--- code/application/models/i18n/db_translation_cache.php
+++ code/application/models/i18n/db_translation_cache.php
@@ -68,7 +68,7 @@ class Db_translation_cache extends CI_Model
 
     if(is_numeric($orig_text)) return $orig_text;
 
-    if (ISWINDOWS || strlen($orig_text) < 10)
+    if (ISWINDOWS)
     {
 		$cacheKey = $lang_code.'-'.md5($orig_text);
 		if ($cache = $this->cache->get($cacheKey))
@@ -102,7 +102,7 @@ class Db_translation_cache extends CI_Model
     {
       log_message('debug', 'Translation Found '.$query->row()->translation);
 
-      if($memcached === true && (ISWINDOWS || strlen($orig_text) < 10))
+      if($memcached === true && (ISWINDOWS)
       {
       	$this->cache->save($cacheKey, $query->row(), 60000);
         return $query->row();
