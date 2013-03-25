78d34e150e52a4c6c0bd83c4a7b3187f470abe0c
Jerome Curlier <jeromecurlier@mcwebmanagement.com>
2013-03-25 15:26:22 -0700

    dev php files

---
+++ apache2/dev/github.php
@@ -0,0 +1,64 @@
+<?php
+
+   // Turn off error reporting
+   error_reporting(0);
+
+   // configuration
+   $log = "/opt/logs/github.log";
+   $payloadLog = "/opt/logs/github_payload.log";
+   $repository = "https://github.com/mcwebmanagement/source";
+   $allowedIPs = array("207.97.227.253", "50.57.128.197", "108.171.174.178", "50.57.231.61");
+   $allowedUsernames = array("jcurlier", "CHRISMORISSEAU");
+   $master = "refs/heads/master";
+   $deployProdString = "deploy to prod";
+   $deployProdScript = "/srv/source/deploy2prod.sh";
+   $deployProdMessage = "deployed to production";
+   $deployDevString = "deploy to dev";
+   $deployDevScript = "/srv/source/deploy2dev.sh";
+   $deployDevMessage = "deployed to dev";
+   $noDeployMessage = "no deployment";
+
+   $IP = $_SERVER['REMOTE_ADDR'];
+   if (in_array ($IP, $allowedIPs)) 
+   {
+      try
+      {
+         // Decode the payload json string
+         $payload = json_decode($_REQUEST['payload']);
+         $reference = $payload->ref;
+         $url = $payload->repository->url;
+         $username = $payload->head_commit->committer->username;
+         $commit = $payload->head_commit->id;
+         $commitMessage =  $payload->head_commit->message;
+
+         // Log the payload object
+         @file_put_contents($payloadLog, print_r($payload, TRUE), FILE_APPEND);
+
+         if ($reference === $master && $url === $repository && (in_array ($username, $allowedUsernames)))
+         {
+
+            if (strpos($commitMessage, $deployProdString) != FALSE)
+            {
+               @file_put_contents($log, date("Y-m-d H:i:s")."\t".$repository."\t".$username."\t".$commit."\t".$deployProdMessage."\n", FILE_APPEND);
+               exec("$deployProdScript");
+            }
+
+            if (strpos($commitMessage, $deployDevString) != FALSE)
+            {
+               @file_put_contents($log, date("Y-m-d H:i:s")."\t".$repository."\t".$username."\t".$commit."\t".$deployDevMessage."\n", FILE_APPEND);
+               exec("$deployDevScript");
+            }
+
+         }
+
+         if ((strpos($commitMessage, $deployProdString) === FALSE) && (strpos($commitMessage, $deployDevString) === FALSE))
+         {
+            @file_put_contents($log, date("Y-m-d H:i:s")."\t".$repository."\t".$username."\t".$commit."\t".$noDeployMessage."\n", FILE_APPEND);
+         }
+     }
+     catch (Exception $e) 
+     {
+         exit;
+     }
+   }
+?>
