270e26170d4785147d0341a20dcdf58dee2d9c07
Jerome Curlier <jeromecurlier@mcwebmanagement.com>
2013-03-25 21:17:47 -0700

    fixed context for cron job

--- scripts/cachehwcity.sh
+++ scripts/cachehwcity.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /opt/scripts
-wget --user=cron --password=hostels --read-timeout=0 http://www.hwsitetest.com/hw_city_list/aEc3FvF6f754Bjida2QMp7gR
+wget --user=cron --password=hostels --read-timeout=0 http://www.hwsitetest.com/cron/hw_city_list/aEc3FvF6f754Bjida2QMp7gR
 rm aEc3FvF6f754Bjida2QMp7gR*
 
