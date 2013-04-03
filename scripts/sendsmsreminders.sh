f54a8bf287dc430a193e19b03a5082b8a2430c73
Jerome Curlier <jeromecurlier@mcwebmanagement.com>
2013-04-03 09:54:11 -0700

    clean up script folder

--- scripts/sendsmsreminders.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /opt/scripts
-wget --user=cron --password=hostels --read-timeout=0 http://www.hwsitetest.com/cron/update_js_city_data/aEc3FvF6f754Bjida2QMp7gR
-rm aEc3FvF6f754Bjida2QMp7gR*
