7362d8b1833e6fbe1718c6d8f06da30128e118f8
Jerome Curlier <jcurlier@yahoo.com>
2013-05-31 20:33:30 +0200

    Update config.php
    deploy to prod

--- code/application/config/config.php
+++ code/application/config/config.php
@@ -29,11 +29,11 @@ $config['recent_view_number_cookies'] = 5;
 /* bing translation log for ON and OFF (TRUE=ON, FALSE=OFF)*/
 if (ISDEVELOPMENT)
 {
-	$config['bingTranslation'] = TRUE;
+	$config['bingTranslation'] = FALSE;
 }
 else
 {
-	$config['bingTranslation'] = TRUE;
+	$config['bingTranslation'] = FALSE;
 }
 
 $config['companies_strings_to_replace'] = array(
