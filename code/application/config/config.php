e9d5bfc6eb97da16e68a8a9ca81a91291eed63d4
Rotich Mordecai <korir.mordecai@gmail.com>
2013-06-03 15:18:06 +0300

    Added booking fees to www.retkeilymajoja.com

--- code/application/config/config.php
+++ code/application/config/config.php
@@ -458,14 +458,16 @@ $config["hwMostPopularFacilitiesById"] = array(
  * HB sites that import Reviews from HW
  */
 $config["hbImporthwReviews"] = array(
-    'www.hbsitetest.com'
+    'www.hbsitetest.com',
+    'www.retkeilymajoja.com'
 );
 
 /*
  * HB sites that charge Booking Fee
  */
 $config["hbChargeBookingFee"] = array(
-    'www.hbsitetest.com'
+    'www.hbsitetest.com',
+    'www.retkeilymajoja.com'
 );
 
 /* End of file config.php */
