241fba9cba74784c33fe65048007bb21334de433
AdrianArnautu <arnautu.adrian@gmail.com>
2013-06-05 00:56:23 +0300

    fixes for MCWEB-587

--- web/ci/js/avail_rooms.js
+++ web/ci/js/avail_rooms.js
@@ -1,60 +1,74 @@
-function checkPropertyRoomsAvail(base_url, propertyNumber, dateId, n, propertyName, currency, dateWarning,minNights, avail_table_id)
-{
+function checkPropertyRoomsAvail(base_url, propertyNumber, dateId, n, propertyName, currency, dateWarning,minNights, avail_table_id) {
+	$("#city_avail_" + propertyNumber).show();
 	$("#avail-overview-"+propertyNumber).hide();
 	$("#loading-dispo-"+propertyNumber).show();
-	if (minNights > n){
+
+	if (minNights > n) {
 		n = minNights;
 	}
+
 	var cards = "";
-	if(document.getElementById('book-property-cards') != null)
-	{
+	if (document.getElementById('book-property-cards') != null) {
 		cards = document.getElementById('book-property-cards').value;
 	}
 
-//	var c=obj.currency.options[obj.currency.selectedIndex].value;
-//	var l=obj.language.value;
+	//	var c=obj.currency.options[obj.currency.selectedIndex].value;
+	//	var l=obj.language.value;
 	var todayDate = new Date();
-  todayDate.setHours(0);
-  todayDate.setMinutes(0);
-  todayDate.setSeconds(0);
-  
-  var searchDate 	=  $("#"+dateId).datepicker( "getDate" );
-  var d = siteDateString(searchDate);
-  
-  searchDate.setHours(23);
-  searchDate.setMinutes(59);
-  searchDate.setSeconds(59);
-  
-	if(isValidDate(d) == false)
-	{
-	  triggerWarning(dateWarning);
-    $("#loading-dispo-"+propertyNumber).hide();
+	todayDate.setHours(0);
+	todayDate.setMinutes(0);
+	todayDate.setSeconds(0);
+
+	var searchDate 	=  $("#"+dateId).datepicker( "getDate" );
+	var d = siteDateString(searchDate);
+
+	searchDate.setHours(23);
+	searchDate.setMinutes(59);
+	searchDate.setSeconds(59);
+
+	if (isValidDate(d) == false) {
+		triggerWarning(dateWarning);
+
+		$("#loading-dispo-"+propertyNumber).hide();
 	}
-	else if(todayDate > searchDate)
-  {
-	  triggerWarning(dateWarning);
-   $("#loading-dispo-"+propertyNumber).hide();
-  }
-	else
-	{
-	  closeWarning();
-	$("#"+avail_table_id).html('');
-	$.ajax({type:"post",
+	else if (todayDate > searchDate) {
+		triggerWarning(dateWarning);
+
+		$("#loading-dispo-" + propertyNumber).hide();
+	}
+	else {
+		closeWarning();
+
+		$("#" + avail_table_id).html('');
+
+		$.ajax({
+			type:"post",
 			url:base_url+"rooms_avail/",
-			data: { propertyNumber: propertyNumber, dateStart: d, numNights: n, propertyName: propertyName, currency:currency, propertyCards: cards },
-			timeout:10000,
-			error:function(XMLHttpRequest, textStatus, errorThrown)
-			{
-				$("#"+avail_table_id).html("<ul class=\"error\"><li>Erreur.<li>"+textStatus+"</li><li>"+errorThrown+"</li></li></ul>");
-				$("#loading-dispo-"+propertyNumber).hide();
+			data: {
+				propertyNumber: propertyNumber,
+				dateStart: d,
+				numNights: n,
+				propertyName: propertyName,
+				currency:currency,
+				propertyCards: cards
 			},
-			success:function(data)
-			{
-				$("#"+avail_table_id).html(data);
-				$("#loading-dispo-"+propertyNumber).hide();
-				
+			timeout:10000,
+			error:function(XMLHttpRequest, textStatus, errorThrown) {
+				$("#" + avail_table_id).html("<ul class=\"error\"><li>Erreur.<li>" + textStatus + "</li><li>" + errorThrown + "</li></li></ul>");
+				$("#loading-dispo-" + propertyNumber).hide();
+			},
+			success:function(data) {
+				$("#show_city_avail_" + propertyNumber).hide();
+				$("#hide_city_avail_" + propertyNumber).show();
+
+				$("#" + avail_table_id).html(data);
+				$("#loading-dispo-" + propertyNumber).hide();
 			}
 		});
 	}
-	
+}
+
+function hidePropertyRoomsAvail(propertyNumber) {
+	$("#show_city_avail_" + propertyNumber).show();
+	$("#hide_city_avail_" + propertyNumber + ', #city_avail_' + propertyNumber).hide();
 }
