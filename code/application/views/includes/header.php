16a944daca78eb513d9b9b79274a39990505a5b8
Rotich Mordecai <korir.mordecai@gmail.com>
2013-06-07 00:31:13 +0300

    header - updated bubble text

--- code/application/views/includes/header.php
+++ code/application/views/includes/header.php
@@ -1019,7 +1019,7 @@ $(document).ready(function()
 					<span class="bubble_blue_right_inner">
                                             <?php if($this->wordpress->get_option('aj_api_site_data') == 'hb'){?>
                                             <?php if($this->wordpress->get_option('aj_hb_charge_booking_fees') == 'true'): ?>
-                                            <?php echo _('Book on your mobile')?>
+                                            <?php printf(gettext("100%% sécurisé."));?>
                                             <?php else: ?>
                                             <?php echo _('No Booking fees')?>
                                             <?php endif; ?>
