b796de6c74baa4bb8a1babcf69de01ce1f7e184b
sasya8080 <sasya8080@gmail.com>
2013-07-11 14:59:06 +0800

    #123, tootip 1st version

--- web/ci/js/jquery.tooltip-sasya.js
+++ web/ci/js/jquery.tooltip-sasya.js
@@ -1,6 +1,7 @@
 //style-my-tootltips by malihu (http://manos.malihu.gr)
 //plugin home http://manos.malihu.gr/style-my-tooltips-jquery-plugin
 (function($){
+
 	var methods={
 		init:function(options){
 			var defaults={ 
