e7d4efd2bb7a4d7b4d7a7f0f0415f2215509ce0b
sasya8080 <sasya8080@gmail.com>
2013-08-07 01:04:03 +0800

    mcweb-586 top and footer flags differences solved.

--- code/application/views/includes/flags_header.php
+++ code/application/views/includes/flags_header.php
@@ -1,13 +1,50 @@
 <div id="language-dropdown" class="dropdown dropdown-tip">
 <ul class="dropdown-menu">
 <?php if ($this->wordpress->get_option('aj_type_site') == "Youth Hostels"){?>
-    <li><a title="NoFeesHostels.com" class="flag-en-us" href="http://www.nofeeshostels.com?currency=USD">NoFeesHostels.com</a></li> 
+    <!--<li><a title="Youth-Hostels.eu" class="flag-eu" href="http://www.youth-hostels.eu">Youth-Hostels.co.uk</a></li>	-->
+    <li><a title="Youth-Hostels.co.uk" class="flag-en" href="http://www.youth-hostels.co.uk">Youth-Hostels.co.uk</a></li>
+    <li><a title="Youth-Hostels.ie" class="flag-ie" href="http://www.youth-hostels.ie">Youth-Hostels.ie</a></li>
+    <li><a title="AubergesDeJeunesse.com" class="flag-fr" href="http://www.aubergesdejeunesse.com">AubergesDeJeunesse.com</a></li>
+    <li><a title="AlberguesJuveniles.es" class="flag-es" href="http://www.alberguesjuveniles.es">AlberguesJuveniles.es</a></li>
+    <li><a title="Jugendherbergen.eu" class="flag-de" href="http://www.jugendherbergen.eu">Jugendherbergen.eu</a></li>
+    <!--<li><a title="PousadasDeJuventude.com" class="flag-pt" href="http://www.pousadasdejuventude.com">PousadasDeJuventude.com</a></li> -->
+    <li><a title="OstelliDellaGioventu.com" class="flag-it" href="http://www.ostellidellagioventu.com">OstelliDellaGioventu.com</a></li>
+    <!--<li><a title="IfjúságiSzállások.com" class="flag-hu" href="http://www.IfjusagiSzallasok.com">IfjúságiSzállások.com</a></li>
+	<li><a title="MládežnickéUbytovny.com" class="flag-cz" href="http://www.mladeznickeubytovny.com">MládežnickéUbytovny.com</a></li>
+	<li><a title="SchroniskaMlodziezowe.com" class="flag-pl" href="http://www.schroniskamlodziezowe.com">SchroniskaMlodziezowe.com</a></li>
+	<li><a title="Youth-Hostels.ru" class="flag-ru" href="http://www.youth-hostels.ru">Youth-Hostels.ru</a></li>
+	<li><a title="Retkeilymajoja.com" class="flag-fi" href="http://www.retkeilymajoja.com">Retkeilymajoja.com</a></li>
+	<?php /*?><li class="clear continent"></li>	<?php */?>
+	<li><a title="Youth-Hostel.com" class="flag-en-us" href="http://www.youth-hostel.com">Youth-Hostel.com</a></li>
+	<li><a title="Youth-Hostels.ca" class="flag-ca" href="http://www.youth-hostels.ca">Youth-Hostels.ca</a></li>
+	<li><a title="AubergesDeJeunesse.ca" class="flag-ca" href="http://www.aubergesdejeunesse.ca">AubergesDeJeunesse.ca</a></li>
+	<li><a title="AlberguesJuveniles.com" class="flag-mx" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
+	<?php /*?><li class="clear continent"></li><?php */?>
+	<li><a title="Youth-Hostels.cn" class="flag-zn" href="http://www.youth-hostels.cn">Youth-Hostels.cn</a></li>
+	<li><a title="Youth-Hostels.hk" class="flag-hk" href="http://www.youth-hostels.hk">Youth-Hostels.hk</a></li>
+	<li><a title="Youth-Hostels.kr" class="flag-kr" href="http://www.youth-hostels.kr">Youth-Hostels.kr</a></li>
+	<li><a title="Youth-Hostels.jp" class="flag-jp" href="http://www.youth-hostels.jp">Youth-Hostels.jp</a></li>
+	<li><a title="Youth-Hostel.com" class="flag-au" href="http://www.youth-hostel.com">Youth-Hostel.com</a></li>
+	<li><a title="Youth-Hostels.co.nz" class="flag-nz" href="http://www.youth-hostels.co.nz">Youth-Hostels.co.nz</a></li>
+	<?php /*?><li class="clear continent"></li>	<?php */?>
+	<li><a title="AlberguesDaJuventude.com" class="flag-br" href="http://www.alberguesdajuventude.com">AlberguesDaJuventude.com</a></li>
+	<li><a title="AlberguesJuveniles.com" class="flag-ar" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
+	<li><a title="AlberguesJuveniles.com" class="flag-pr" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
+	<li><a title="AlberguesJuveniles.com" class="flag-cb" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
+	<li><a title="AlberguesJuveniles.com" class="flag-cl" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
+	<li><a title="AlberguesJuveniles.com" class="flag-uy" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
+	<li><a title="AlberguesJuveniles.com" class="flag-py" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
+	<li><a title="AlberguesJuveniles.com" class="flag-bo" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
+	<li><a title="AlberguesJuveniles.com" class="flag-cl" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
+	-->
+<?php }else{ ?>
+    <li><a title="NoFeesHostels.com" class="flag-en-us" href="http://www.nofeeshostels.com?currency=USD">NoFeesHostels.com</a></li>
     <li><a title="NoFeesHostels.com" class="flag-en" href="http://www.nofeeshostels.com?currency=GBP">NoFeesHostels.com</a></li>
     <li><a title="NoFeesHostels.com" class="flag-eu" href="http://www.nofeeshostels.com?currency=EUR">NoFeesHostels.com</a></li>
     <li><a title="Auberges.com" class="flag-fr" href="http://www.auberges.com">Auberges.com</a></li>
     <li><a title="Hostales.com" class="flag-es" href="http://www.hostales.com">Hostales.com</a></li>
     <!--<li><a title="Hostels.in" class="flag-in" href="http://www.hostels.in">Hostels.in</a></li>-->
-    <li><a title="Herbergen.com" class="flag-de" href="http://www.herbergen.com">Herbergen.com</a></li>    
+    <li><a title="Herbergen.com" class="flag-de" href="http://www.herbergen.com">Herbergen.com</a></li>
     <li><a title="Hostelli.com" class="flag-it" href="http://www.hostelli.com">Hostelli.com</a></li>
     <!--<li><a title="Hostelek.com" class="flag-hu" href="http://www.hostelek.com">Hostelek.com</a></li>
     <li><a title="Hostelleja.com" class="flag-fi" href="http://www.hostelleja.com">Hostelleja.com</a></li>
@@ -15,44 +52,9 @@
     <li><a title="Hostely.com" class="flag-cz" href="http://www.hostely.com">Hostely.com</a></li>
     <li><a title="Hostéis.com" class="flag-pt" href="http://www.hosteis.com/?currency=EUR">Hostéis.com</a></li>
     <li><a title="Hostéis.com" class="flag-br" href="http://www.hosteis.com?currency=BRL">Hostéis.com</a></li>
-    <li><a title="хостелы.рф" class="flag-ru" href="http://www.хостелы.рф">хостелы.рф</a></li>  
-    <li><a title="Hostels.jp" class="flag-jp" href="http://www.hostels.jp/">Hostels.jp</a></li>     
-    <li><a title="저렴한호스텔.kr" class="flag-kr" href="http://XN--XN2BY4QTJE86KN5EZMB.KR/">저렴한호스텔.kr</a></li> -->        
-<?php }else{ ?>
-    <!--<li><a title="Youth-Hostels.eu" class="flag-eu" href="http://www.youth-hostels.eu">Youth-Hostels.co.uk</a></li> --> 
-    <li><a title="Youth-Hostels.co.uk" class="flag-en" href="http://www.youth-hostels.co.uk">Youth-Hostels.co.uk</a></li>
-    <li><a title="Youth-Hostels.ie" class="flag-ie" href="http://www.youth-hostels.ie">Youth-Hostels.ie</a></li>            
-    <li><a title="AubergesDeJeunesse.com" class="flag-fr" href="http://www.aubergesdejeunesse.com">AubergesDeJeunesse.com</a></li>  
-    <li><a title="AlberguesJuveniles.es" class="flag-es" href="http://www.alberguesjuveniles.es">AlberguesJuveniles.es</a></li>
-    <li><a title="Jugendherbergen.eu" class="flag-de" href="http://www.jugendherbergen.eu">Jugendherbergen.eu</a></li>
-    <!--<li><a title="PousadasDeJuventude.com" class="flag-pt" href="http://www.pousadasdejuventude.com">PousadasDeJuventude.com</a></li> -->
-    <li><a title="OstelliDellaGioventu.com" class="flag-it" href="http://www.ostellidellagioventu.com">OstelliDellaGioventu.com</a></li>    
-    <!--<li><a title="IfjúságiSzállások.com" class="flag-hu" href="http://www.IfjusagiSzallasok.com">IfjúságiSzállások.com</a></li>
-    <li><a title="MládežnickéUbytovny.com" class="flag-cz" href="http://www.mladeznickeubytovny.com">MládežnickéUbytovny.com</a></li>   
-    <li><a title="SchroniskaMlodziezowe.com" class="flag-pl" href="http://www.schroniskamlodziezowe.com">SchroniskaMlodziezowe.com</a></li>
-    <li><a title="Youth-Hostels.ru" class="flag-ru" href="http://www.youth-hostels.ru">Youth-Hostels.ru</a></li>  
-    <li><a title="Retkeilymajoja.com" class="flag-fi" href="http://www.retkeilymajoja.com">Retkeilymajoja.com</a></li>
-        <li><a title="Youth-Hostel.com" class="flag-en-us" href="http://www.youth-hostel.com">Youth-Hostel.com</a></li>
-    <li><a title="Youth-Hostels.ca" class="flag-ca" href="http://www.youth-hostels.ca">Youth-Hostels.ca</a></li>
-    <li><a title="AubergesDeJeunesse.ca" class="flag-ca" href="http://www.aubergesdejeunesse.ca">AubergesDeJeunesse.ca</a></li>
-    <li><a title="AlberguesJuveniles.com" class="flag-mx" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
-        <li><a title="Youth-Hostels.cn" class="flag-zn" href="http://www.youth-hostels.cn">Youth-Hostels.cn</a></li>
-    <li><a title="Youth-Hostels.hk" class="flag-hk" href="http://www.youth-hostels.hk">Youth-Hostels.hk</a></li>
-    <li><a title="Youth-Hostels.kr" class="flag-kr" href="http://www.youth-hostels.kr">Youth-Hostels.kr</a></li>
-    <li><a title="Youth-Hostels.jp" class="flag-jp" href="http://www.youth-hostels.jp">Youth-Hostels.jp</a></li>
-    <li><a title="Youth-Hostel.com" class="flag-au" href="http://www.youth-hostel.com">Youth-Hostel.com</a></li>
-    <li><a title="Youth-Hostels.co.nz" class="flag-nz" href="http://www.youth-hostels.co.nz">Youth-Hostels.co.nz</a></li>
-        <li><a title="AlberguesDaJuventude.com" class="flag-br" href="http://www.alberguesdajuventude.com">AlberguesDaJuventude.com</a></li>
-    <li><a title="AlberguesJuveniles.com" class="flag-ar" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>  
-    <li><a title="AlberguesJuveniles.com" class="flag-pr" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
-    <li><a title="AlberguesJuveniles.com" class="flag-cb" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
-    <li><a title="AlberguesJuveniles.com" class="flag-cl" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
-    <li><a title="AlberguesJuveniles.com" class="flag-uy" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
-    <li><a title="AlberguesJuveniles.com" class="flag-py" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
-    <li><a title="AlberguesJuveniles.com" class="flag-bo" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
-    <li><a title="AlberguesJuveniles.com" class="flag-cl" href="http://www.alberguesjuveniles.com">AlberguesJuveniles.com</a></li>
-    -->
-
+    <li><a title="хостелы.рф" class="flag-ru" href="http://www.хостелы.рф">хостелы.рф</a></li>
+    <li><a title="Hostels.jp" class="flag-jp" href="http://www.hostels.jp/">Hostels.jp</a></li>
+    <li><a title="저렴한호스텔.kr" class="flag-kr" href="http://XN--XN2BY4QTJE86KN5EZMB.KR/">저렴한호스텔.kr</a></li> -->
 <?php } ?>
 </ul>
 </div>
